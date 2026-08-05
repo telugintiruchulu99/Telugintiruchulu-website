28d06fa56086f62a9a0d69d8248ba4364eae02b1
telugintiruchulu99 <telugintiruchulu99@gmail.com>
2026-08-05 16:56:01 +0530

    Connect frontend to Render backend

--- src/api.js
+++ src/api.js
@@ -1,5 +1,5 @@
 export async function createOrder(amount) {
-  const response = await fetch("http://localhost:5000/create-order", {
+  const response = await fetch("https://telugintiruchulu-website.onrender.com/create-order", {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
@@ -19,7 +19,7 @@ export async function createOrder(amount) {
 export async function verifyPayment(data) {
   console.log("Sending to backend:", data);
 
-  const response = await fetch("http://localhost:5000/verify-payment", {
+  const response = await fetch("https://telugintiruchulu-website.onrender.com/verify-payment", {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
